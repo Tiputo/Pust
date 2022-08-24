7db82acdc1154f190c25a015d928717a59402de7
Tiputo <parekhonza@gmail.com>
2022-08-24 10:00:07 +0200

    Created model for completion

--- api/model/Records.ts
+++ api/model/Records.ts
@@ -4,5 +4,7 @@ import { User } from "./User"
 export class Records {
     user = def.manyHasOne(User, "records").cascadeOnDelete()
     lastIn = def.dateTimeColumn().default('now')
-
+    yes = def.stringColumn().notNull()
+    almost = def.stringColumn().notNull()
+    no = def.stringColumn().notNull()
 }
